34c2f3d09aff0d9b8570fe85c26356c347653c10
Richi Garza <richi.garzar@gmail.com>
2017-02-11 00:07:51 -0600

    Se crea script sobre la información que se va cargar al sistema

---
+++ SQL/TEST_INFO.sql
@@ -0,0 +1,4 @@
+INSERT INTO Band (bandName, bandGenre) VALUES ('Metallica', 'Metal');
+INSERT INTO User (firstName, lastName, userName, email, location, userType) VALUES('Richi', 'Garza', 'RichiGarza', '', 'MTY', 2);
+INSERT INTO Venue (venueName, location) VALUES('Estadio BBVA Bancomer', 'Guadalupe, Nuevo León'); 
+INSERT INTO Event (eventName, idVenue, userName, idBand, votes, VotesReached) VALUES('Corona North Side', 1, 1, 1, 10000, 0);
