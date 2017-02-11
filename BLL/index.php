a81c28c11ad28dd225641aaafca373a255cc78f1
Richi Garza <richi.garzar@gmail.com>
2017-02-10 20:34:52 -0600

    Se corrige error en instancia de band

--- BLL/index.php
+++ BLL/index.php
@@ -2,7 +2,7 @@
 /////////////////////////////
 // index.php
 // Autor: Richi Garza
-// Fecha: 07-feb-2017
+// Fecha: 09-feb-2017
 ////////////////////////////
 	header('Content-type: application/json');
 	require('../DTO/User.php');
@@ -16,26 +16,28 @@
 
 	function lol(){
 		$user = new User('Ricardo', 'Garza', 'Richi', 'richi@conglom-osoft.net', 'Monterrey, MX', '19-02-2016', '19-02-2016', NULL);
-		$band = new Band('Metallica', 'Metal', '19-02-2016');	
-		$venue = new Venue('Estadio BBVA Bancomer', 'Guadalupe, MX', '19-02-2016');
+		$band = new Band('Metallica', 'Metal', '19-02-2016', '19-02-2016');	
+		$venue = new Venue('Estadio BBVA Bancomer', 'Guadalupe, MX', '19-02-2016', '19-02-2016');
 		$event = new Event('Metallica en Monterrey', $venue, $user, $band, '50000', '0', '19-02-2016', '19-02-2016');
 
 		$user->setPassword('la4705');
 		return $user->getPassword();
 	}
 
-	$action = $_GET["action"];
-	switch($action){
-		case "test":
-			$result["success"] = test();
-		break;
-		case "lol":
-			$result["success"] = lol();
-		break;
-		default:
-			$result["success"] = false;
+	function main(){
+		$action = $_GET["action"];
+		switch($action){
+			case "test":
+				$result["success"] = test();
+			break;
+			case "lol":
+				$result["success"] = lol();
+			break;
+			default:
+				$result["success"] = false;
+		}
+		echo json_encode($result);
 	}
-
-	echo json_encode($result);
+	main();
 	exit();
 ?>
